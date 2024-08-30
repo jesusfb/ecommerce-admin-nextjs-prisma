bd55859277aa50c288b813a7ac9a7a125b3677c4
Varunjoshi22 <92745621+Varunjoshi22@users.noreply.github.com>
2024-08-30 22:36:07 +0530

    updated Readme file

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
 import { useStoreModalStore } from "@/hooks/use-store-modal";
-import { SignedIn, UserButton, SignedOut, SignInButton } from "@clerk/nextjs";
 import { useEffect } from "react";
 
 const SetupPage = () =>{
